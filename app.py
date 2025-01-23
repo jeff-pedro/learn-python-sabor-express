45f8b843f8efc586b886e66c2a0622c0df60c66f
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-01-23 00:14:53 -0300

    menu de opcoes

---
+++ app.py
@@ -0,0 +1,13 @@
+print('''
+█▀ █▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▄█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+''')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+print(f'Você escolheu a opcao {opcao_escolhida}')
