64b38f0caf7e7b5737efcfd10b6d2a6a8e1b0949
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-01-24 23:18:26 -0300

    implementando método __str__ na classe Restaurante

--- models/restautante.py
+++ models/restautante.py
@@ -4,13 +4,16 @@ class Restaurante:
         self.categoria = categoria
         self.ativo = ativo
 
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}' 
+
     
 
 restaurante_guaco = Restaurante('Guaco', 'Fast-Food')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-print(vars(restaurante_guaco))
-print(vars(restaurante_pizza))
+print(restaurante_guaco)
+print(restaurante_pizza)
 
 # print(vars(restaurante_guaco)) # vars -> dicionário com as propriedades do objeto
 # print(dir(restaurante_guaco)) # dir -> lista com todos os atributos e métodos do objeto
