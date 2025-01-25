9e590ebdb2135959803239c77dc674bfaf996dbc
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-01-25 12:13:44 -0300

    torna privado os atributos e formata entrada de dados

--- models/restautante.py
+++ models/restautante.py
@@ -2,20 +2,20 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria, ativo = False):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.title()
         self._ativo = ativo
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
     def listar():
         print(f'{"NOME DO RESTAURANTE".ljust(25)} | {"CATEGORIA".ljust(25)} | STATUS')
         print(f'{"-" * 25} | {"-" * 25} | {"-" * 9}')
         
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
         
         return ''
     
@@ -28,10 +28,10 @@ class Restaurante:
     @property
     def ativo(self):
         return '☒' if self._ativo else '❏'
-    
 
-restaurante_guaco = Restaurante('Guaco', 'Fast-Food')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+restaurante_guaco = Restaurante('guaco', 'fast-food')
+restaurante_pizza = Restaurante('pizza express', 'italiana')
 
 print(restaurante_guaco.alterar_status())
 
