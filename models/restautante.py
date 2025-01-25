e412ab65a6c583e47daf658f106de3ed6939d8ce
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-01-24 23:10:13 -0300

    adiciona construtor na classe Restaurante

--- models/restautante.py
+++ models/restautante.py
@@ -1,11 +1,16 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria, ativo = False):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = ativo
 
-restaurante_guaco = Restaurante()
-restaurante_guaco.nome = 'Guaco'
-restaurante_guaco.categoria = 'fast-food'
+    
 
-print(vars(restaurante_guaco)) # vars -> dicionário com as propriedades do objeto
-print(dir(restaurante_guaco)) # dir -> lista com todos os atributos e métodos do objeto
+restaurante_guaco = Restaurante('Guaco', 'Fast-Food')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+print(vars(restaurante_guaco))
+print(vars(restaurante_pizza))
+
+# print(vars(restaurante_guaco)) # vars -> dicionário com as propriedades do objeto
+# print(dir(restaurante_guaco)) # dir -> lista com todos os atributos e métodos do objeto
